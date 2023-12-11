84eda4b72b6b0176bedfa42b20f48fd7a71cbe54
swathi.goli <swathi.goli@TNN-5CG3247N1J>
2023-12-11 22:44:17 +1300

    Add project files.

---
+++ FormSubmit/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using FormSubmit.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Text;
+
+namespace FormSubmit.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult UserForm()
+        {
+            return View(new UserViewModel());
+        }
+
+        [HttpPost]
+        public IActionResult UserForm(UserViewModel uvm)
+        {
+            uvm.Id = Guid.NewGuid();
+            string jsonObject = JsonConvert.SerializeObject(uvm);
+            string filePath = _configuration.GetValue<string>("FilePath");
+
+            if (System.IO.File.Exists(filePath))
+                System.IO.File.WriteAllText(filePath, jsonObject);
+            else
+            {
+                using (var file = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.Write))
+                {
+                    using (var writer = new StreamWriter(file, Encoding.UTF8))
+                    {
+                        writer.Close();
+                        file.Close();
+                        System.IO.File.WriteAllText(filePath, jsonObject);
+                    }
+                }
+                }
+               
+            ViewBag.SuccessMessage = " Record has been submitted successfully!";
+            ModelState.Clear();
+
+            return View(new UserViewModel());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FormSubmit/Models/UserViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace FormSubmit.Models
+{
+    public class UserViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [Display(Name = "Fast Name")]
+        public string FirstName { set; get; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set;}
+    }
+}
